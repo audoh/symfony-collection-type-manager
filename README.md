b38e5e85eeb87c1c525c91e68fd6a203c3e717d9
Ashleigh Udoh <mail@audoh.co.uk>
2017-03-14 19:17:05 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# CollectionType Manager
+JQuery plugin to handle addition to and removal from Symfony CollectionTypes with minimal effort.
+
+## Installation and usage
+
+The easiest way to get started with the CollectionType Manager is to simply include the script in the page containing your form. It will automatically insert 'add' and 'delete' buttons into any form fragment containing either a `data-prototype` or the attribute `data-allow-remove="true"`.
+
+## Features
+
+- Create custom add and delete buttons by setting `data-adder-prototype` and `data-deleter-prototype`. The default is simply a button saying 'add' or 'delete'.
+- Change the child tag using `data-child-tag`. The default is 'div'.
+- Listen to `collection_add` and `collection_delete` events to react to user actions in other scripts.
+- Control whether to allow addition or deletion using the boolean attributes `data-allow-add` and `data-allow-delete`.
+
+## Future plans
+
+### Greater flexibility
+
+At present, the plugin currently assumes that the `data-prototype` attribute is on the immediate parent of a set of child elements, each of which represents an item in the CollectionType. This means that if you deviate from this, or have non-collection item elements within the parent element, the plugin may behave in unexpected ways.
+
+## Notes
+
+This plugin uses the two class names `collection_add` and `collection_delete` to identify existing add and delete buttons, which you should avoid using within the parent element. This applies even when you use custom button prototypes.
---
+++ collection_type-manager.jquery.js
@@ -0,0 +1,214 @@
+/* CollectionType-Manager by A. Udoh | MIT */
+/* General-purpose solution for adding to and removing from Symfony CollectionTypes */
+(function($)
+{ 
+	$.fn.collectionManager = function(fn, args)
+	{
+		return fnMap[fn].apply(this, args);
+	};
+
+	var fnMap =
+	{
+		'isDeleteAllowed': function()
+		{
+			return $(this).data('allowDelete') === false ? false : true;
+		},
+
+		'isAddAllowed': function()
+		{
+			// Addition is never allowed if there's no prototype.
+
+			if($(this).data('prototype') === undefined)
+				return false;
+
+			return $(this).data('allowAdd') === false ? false : true;
+		},
+
+		'childTag': function()
+		{
+			return $(this).data('childTag') !== undefined ? $(this).data('childTag') : 'div';
+		},
+
+		// Add a new item to an element based on its data-prototype (from Symfony).
+
+		'addCollectionItem': function(parent)
+		{
+			// Get prototype data.
+
+			var prototype = $(parent).data('prototype');
+			var prototypeName = fnMap.resolvePrototypeName(prototype);
+
+			var childTag = $(parent).collectionManager('childTag');
+
+			// Build prototype with new id.
+
+			var id = $(parent).children(childTag).length;
+
+	        // Replace __name__ with id.
+
+	        var matchName = new RegExp(prototypeName, 'g');
+	        var prototype = prototype.replace(matchName, id);
+
+			// Build new element from the prototype.
+
+			var item = $('<' + childTag + '/>').html($(prototype).html());
+
+			item.insertBefore($(this))
+
+			// Trigger add event.
+
+			$(parent).trigger('collection_add')
+
+			// Give the new element a delete button if allowed and update on it.
+
+			if($(parent).collectionManager('isDeleteAllowed'))
+				$(item).collectionManager('provideDeleteButton');
+
+			$(item).collectionManager('updateCollection');
+		},
+
+		// Find the name of a prototype from the prototype string.
+
+		'resolvePrototypeName': function(prototype)
+		{
+			return prototype.match(/__[^_\s]*__/);
+		},
+
+		// Return an add button element.
+
+		'newAddButton': function()
+		{
+			// Check for add button prototype in this element.
+
+			var button, prototype = $(this).data('adderPrototype');
+
+			if(prototype !== undefined)
+			{
+				button = $(prototype);
+			}
+			else
+			{
+				button = $('<button/>')
+					
+					.attr('type', 'button')
+					.html('add')
+				;
+			}
+
+			button.addClass('collection_add');
+
+			return button;
+		},
+
+		// Return a delete button element.
+
+		'newDeleteButton': function()
+		{
+			// Check for delete button prototype in parent.
+
+			var button, prototype = $(this).parent().data('deleterPrototype');
+
+			if(prototype !== undefined)
+			{
+				button = $(prototype);
+			}
+			else
+			{
+				button = $('<button/>')
+					
+					.attr('type', 'button')
+					.html('delete')
+				;
+			}
+
+			button.addClass('collection_delete');
+
+			return button;
+		},
+
+		// Creates an add button on `element` if one does not already exist.
+
+		'provideAddButton': function()
+		{
+			var needed = $(this).find('> .collection_add').length == 0;
+
+			if(needed)
+			{
+				// Create add button.
+
+				var parent = this;
+
+				addButton = $(this).collectionManager('newAddButton')
+					.appendTo(this)
+					.click(function()
+					{
+						$(this).collectionManager('addCollectionItem', parent);
+					})
+				;
+			}
+		},
+
+		// Creates a delete button on `element` if one does not already exist.
+
+		'provideDeleteButton': function()
+		{
+			var needed = $(this).find('> .collection_delete').length == 0;
+
+			if(needed)
+			{
+				var deletable = this;
+
+				deleteButton = $(this).collectionManager('newDeleteButton')
+					.appendTo(this)
+					.click(function()
+					{
+						$(deletable).remove();
+					})
+				;
+			}
+		},
+
+		// Updates stuff.
+
+		'updateCollection': function(childSelector)
+		{
+			// Get all applicable forms/form fragments.
+
+			var elements = $(this).find('[data-prototype], [data-allow-remove="true"]');
+
+			// Iterate over each one, adding 'add' and 'delete' controls.
+
+			elements.each(function()
+			{
+				// Create 'add' button if needed and allowed.
+
+				if($(this).collectionManager('isAddAllowed'))
+					$(this).collectionManager('provideAddButton');
+
+				// Create 'delete' buttons if needed and allowed.
+
+				if($(this).collectionManager('isDeleteAllowed'))
+				{
+					if(childSelector === undefined)
+					{
+						var childTag = $(parent).collectionManager('childTag');
+
+						childSelector = '> ' + childTag;
+					}
+
+					var children = $(this).find(childSelector);
+
+					children.each(function()
+					{
+						$(this).collectionManager('provideDeleteButton');
+					})
+				}				
+			});
+		}
+	}
+
+	$(document).ready(function()
+	{
+		$(this).collectionManager('updateCollection');
+	});
+}(jQuery));
